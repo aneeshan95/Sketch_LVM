fdbe9fc029bdda09ddafdad6080ed3ba4ac0bc1e
Aneeshan Sain <56027547+aneeshan95@users.noreply.github.com>
2023-07-28 13:24:34 +0100

    Add files via upload

---
+++ src/dataset_retrieval.py
@@ -0,0 +1,122 @@
+import os
+import glob
+import numpy as np
+import torch
+from torchvision import transforms
+from PIL import Image, ImageOps
+
+unseen_classes = [
+    "bat",
+    "cabin",
+    "cow",
+    "dolphin",
+    "door",
+    "giraffe",
+    "helicopter",
+    "mouse",
+    "pear",
+    "raccoon",
+    "rhinoceros",
+    "saw",
+    "scissors",
+    "seagull",
+    "skyscraper",
+    "songbird",
+    "sword",
+    "tree",
+    "wheelchair",
+    "windmill",
+    "window",
+]
+
+class Sketchy(torch.utils.data.Dataset):
+
+    def __init__(self, opts, transform, mode='train', used_cat=None, return_orig=False):
+
+        self.opts = opts
+        self.transform = transform
+        self.return_orig = return_orig
+
+        self.all_categories = os.listdir(os.path.join(self.opts.data_dir, 'sketch'))
+        if '.ipynb_checkpoints' in self.all_categories:
+            self.all_categories.remove('.ipynb_checkpoints')
+            
+        if self.opts.data_split > 0:
+            np.random.shuffle(self.all_categories)
+            if used_cat is None:
+                self.all_categories = self.all_categories[:int(len(self.all_categories)*self.opts.data_split)]
+            else:
+                self.all_categories = list(set(self.all_categories) - set(used_cat))
+        else:
+            if mode == 'train':
+                self.all_categories = list(set(self.all_categories) - set(unseen_classes))
+            else:
+                self.all_categories = unseen_classes
+
+        self.all_sketches_path = []
+        self.all_photos_path = {}
+
+        for category in self.all_categories:
+            self.all_sketches_path.extend(glob.glob(os.path.join(self.opts.data_dir, 'sketch', category, '*.png')))
+            self.all_photos_path[category] = glob.glob(os.path.join(self.opts.data_dir, 'photo', category, '*.jpg'))
+
+    def __len__(self):
+        return len(self.all_sketches_path)
+        
+    def __getitem__(self, index):
+        filepath = self.all_sketches_path[index]                
+        category = filepath.split(os.path.sep)[-2]
+        filename = os.path.basename(filepath)
+        
+        neg_classes = self.all_categories.copy()
+        neg_classes.remove(category)
+
+        sk_path  = filepath
+        img_path = np.random.choice(self.all_photos_path[category])
+        neg_path = np.random.choice(self.all_photos_path[np.random.choice(neg_classes)])
+
+        sk_data  = ImageOps.pad(Image.open(sk_path).convert('RGB'),  size=(self.opts.max_size, self.opts.max_size))
+        img_data = ImageOps.pad(Image.open(img_path).convert('RGB'), size=(self.opts.max_size, self.opts.max_size))
+        neg_data = ImageOps.pad(Image.open(neg_path).convert('RGB'), size=(self.opts.max_size, self.opts.max_size))
+
+        sk_tensor  = self.transform(sk_data)
+        img_tensor = self.transform(img_data)
+        neg_tensor = self.transform(neg_data)
+        
+        if self.return_orig:
+            return (sk_tensor, img_tensor, neg_tensor, category, filename,
+                sk_data, img_data, neg_data)
+        else:
+            return (sk_tensor, img_tensor, neg_tensor, category, filename)
+
+    @staticmethod
+    def data_transform(opts):
+        dataset_transforms = transforms.Compose([
+            transforms.Resize((opts.max_size, opts.max_size)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+        return dataset_transforms
+
+
+if __name__ == '__main__':
+    from experiments.options import opts
+    import tqdm
+
+    dataset_transforms = Sketchy.data_transform(opts)
+    dataset_train = Sketchy(opts, dataset_transforms, mode='train', return_orig=True)
+    dataset_val = Sketchy(opts, dataset_transforms, mode='val', used_cat=dataset_train.all_categories, return_orig=True)
+
+    idx = 0
+    for data in tqdm.tqdm(dataset_val):
+        continue
+        (sk_tensor, img_tensor, neg_tensor, filename,
+            sk_data, img_data, neg_data) = data
+
+        canvas = Image.new('RGB', (224*3, 224))
+        offset = 0
+        for im in [sk_data, img_data, neg_data]:
+            canvas.paste(im, (offset, 0))
+            offset += im.size[0]
+        canvas.save('output/%d.jpg'%idx)
+        idx += 1
---
+++ src/model_LN_prompt.py
@@ -0,0 +1,97 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchmetrics.functional import retrieval_average_precision
+import pytorch_lightning as pl
+
+from src.clip import clip
+from experiments.options import opts
+
+def freeze_model(m):
+    m.requires_grad_(False)
+
+def freeze_all_but_bn(m):
+    if not isinstance(m, torch.nn.LayerNorm):
+        if hasattr(m, 'weight') and m.weight is not None:
+            m.weight.requires_grad_(False)
+        if hasattr(m, 'bias') and m.bias is not None:
+            m.bias.requires_grad_(False)
+
+class Model(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+
+        self.opts = opts
+        self.clip, _ = clip.load('ViT-B/32', device=self.device)
+        self.clip.apply(freeze_all_but_bn)
+
+        # Prompt Engineering
+        self.sk_prompt = nn.Parameter(torch.randn(self.opts.n_prompts, self.opts.prompt_dim))
+        self.img_prompt = nn.Parameter(torch.randn(self.opts.n_prompts, self.opts.prompt_dim))
+
+        self.distance_fn = lambda x, y: 1.0 - F.cosine_similarity(x, y)
+        self.loss_fn = nn.TripletMarginWithDistanceLoss(
+            distance_function=self.distance_fn, margin=0.2)
+
+        self.best_metric = -1e3
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam([
+            {'params': self.clip.parameters(), 'lr': self.opts.clip_LN_lr},
+            {'params': [self.sk_prompt] + [self.img_prompt], 'lr': self.opts.prompt_lr}])
+        return optimizer
+
+    def forward(self, data, dtype='image'):
+        if dtype == 'image':
+            feat = self.clip.encode_image(
+                data, self.img_prompt.expand(data.shape[0], -1, -1))
+        else:
+            feat = self.clip.encode_image(
+                data, self.sk_prompt.expand(data.shape[0], -1, -1))
+        return feat
+
+    def training_step(self, batch, batch_idx):
+        sk_tensor, img_tensor, neg_tensor, category = batch[:4]
+        img_feat = self.forward(img_tensor, dtype='image')
+        sk_feat = self.forward(sk_tensor, dtype='sketch')
+        neg_feat = self.forward(neg_tensor, dtype='image')
+
+        loss = self.loss_fn(sk_feat, img_feat, neg_feat)
+        self.log('train_loss', loss)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        sk_tensor, img_tensor, neg_tensor, category = batch[:4]
+        img_feat = self.forward(img_tensor, dtype='image')
+        sk_feat = self.forward(sk_tensor, dtype='sketch')
+        neg_feat = self.forward(neg_tensor, dtype='image')
+
+        loss = self.loss_fn(sk_feat, img_feat, neg_feat)
+        self.log('val_loss', loss)
+        return sk_feat, img_feat, category
+
+    def validation_epoch_end(self, val_step_outputs):
+        Len = len(val_step_outputs)
+        if Len == 0:
+            return
+        query_feat_all = torch.cat([val_step_outputs[i][0] for i in range(Len)])
+        gallery_feat_all = torch.cat([val_step_outputs[i][1] for i in range(Len)])
+        all_category = np.array(sum([list(val_step_outputs[i][2]) for i in range(Len)], []))
+
+
+        ## mAP category-level SBIR Metrics
+        gallery = gallery_feat_all
+        ap = torch.zeros(len(query_feat_all))
+        for idx, sk_feat in enumerate(query_feat_all):
+            category = all_category[idx]
+            distance = -1*self.distance_fn(sk_feat.unsqueeze(0), gallery)
+            target = torch.zeros(len(gallery), dtype=torch.bool)
+            target[np.where(all_category == category)] = True
+            ap[idx] = retrieval_average_precision(distance.cpu(), target.cpu())
+        
+        mAP = torch.mean(ap)
+        self.log('mAP', mAP)
+        if self.global_step > 0:
+            self.best_metric = self.best_metric if  (self.best_metric > mAP.item()) else mAP.item()
+        print ('mAP: {}, Best mAP: {}'.format(mAP.item(), self.best_metric))
